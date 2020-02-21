2f74ccd06e8821c14666b079db4291c231cbd982
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-21 05:22:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,15 +7,20 @@ class Song
   def initialize(name,artist,genre)
     @name = name
     
-    if !@@artists.include?(artist)
-      @artist = artist
-      @@artists << artist
-    end
+    # if !@@artists.include?(artist)
+    #   @artist = artist
+    #   @@artists << artist
+    # end
     
-    if !@@genres.include?(genre)
-      @genre = genre
-      @@genres << genre 
-    end
+    # if !@@genres.include?(genre)
+    #   @genre = genre
+    #   @@genres << genre 
+    # end
+    
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre 
     @@count +=1
   end
   
