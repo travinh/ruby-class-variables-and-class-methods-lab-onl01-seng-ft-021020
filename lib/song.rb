95c34c0460da41c04cebde94295809983fabc2b9
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-21 04:58:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song 
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@genres = []
-  @@artists = []
+  
+  
   
   def initialize(name,artist,genre)
+    @@artists = []
+    @@genres = []
     @name = name
     
     if !@@artists.include?(artist)
